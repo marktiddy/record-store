308f253bc493dfdc08fe68645f1c3b3fc80c3df3
Mark Tiddy <marktiddy@gmail.com>
2020-07-15 16:01:23 +0100

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const links = document.querySelectorAll(".main-text a");
+
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+  scroll({
+    top: offsetTop,
+    behavior: "smooth",
+  });
+}
